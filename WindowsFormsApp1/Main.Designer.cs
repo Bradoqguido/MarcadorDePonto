655e5b5781d2a1c04a3cd683b7da00ae0bf50cfd
Jeferson Eduardo Guido <bradoqguido@gmail.com>
2020-04-30 17:40:57 -0300

    Commit com o projeto quase terminado.

---
+++ WindowsFormsApp1/Main.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBaterPonto = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBaterPonto
+            // 
+            this.btnBaterPonto.Location = new System.Drawing.Point(49, 12);
+            this.btnBaterPonto.Name = "btnBaterPonto";
+            this.btnBaterPonto.Size = new System.Drawing.Size(96, 23);
+            this.btnBaterPonto.TabIndex = 1;
+            this.btnBaterPonto.Text = "Marcar ponto";
+            this.btnBaterPonto.UseVisualStyleBackColor = true;
+            this.btnBaterPonto.Click += new System.EventHandler(this.btnBaterPonto_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(58, 41);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 3;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(58, 70);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 4;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(197, 112);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnBaterPonto);
+            this.Name = "Main";
+            this.Text = "Auto ponto";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        
+        private System.Windows.Forms.Button btnBaterPonto;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ WindowsFormsApp1/Models/Ponto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Models
+{
+    public class Ponto
+    {
+
+        /// <summary>
+        /// Salva o dia atual
+        /// </summary>
+        private DateTime dtaData = new DateTime();
+
+        /// <summary>
+        /// Variável para armazenar os horarios
+        /// </summary>
+        private List<string> lstHorarios = new List<string>();
+
+        /// <summary>
+        /// Get/Set com o tipo data
+        /// </summary>
+        public DateTime Data
+        {
+            set { dtaData = value; }
+            get { return dtaData; }
+        }
+
+        /// <summary>
+        /// Insere horarios na lista
+        /// e sempre retorna o ultimo item adicionado
+        /// </summary>
+        public string LastHorarioRegistrado
+        {
+            set { lstHorarios.Add(value); }
+            get { return lstHorarios.Last(); }
+        }
+
+        /// <summary>
+        /// Recupera os horarios
+        /// </summary>
+        public List<string> Horarios
+        {
+            get { return lstHorarios; }
+        }
+    }
+}
